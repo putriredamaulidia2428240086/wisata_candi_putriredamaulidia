2c2aee9d5ce29a954d04ced11dcf4172f302dad0
putriredamaulidia2428240086 <putriredamaulidia_2428240086@mhs.mdp.ac.id>
2025-11-24 12:56:18 +0700

    update

--- lib/widgets/item_card.dart
+++ lib/widgets/item_card.dart
@@ -1,66 +1,66 @@
 import 'package:flutter/material.dart';
-
-import '/models/candi.dart';
+import 'package:wisata_candi/models/candi.dart';
+import 'package:wisata_candi/screens/detail_screen.dart';
 
 class ItemCard extends StatelessWidget {
+//   TODO: 1. Deklarasikan variabel yang dibutuhkan dan pasang pada konstruktor
   final Candi candi;
-  final VoidCallback? onTap;
 
   const ItemCard({
     super.key,
     required this.candi,
-    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
+    // TODO: 6. Implementasi routing ke DetailScreen
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(candi: candi),
+          ),
+        );
+      },
       child: Card(
-        elevation: 3,
-        margin: const EdgeInsets.all(8),
+        //   TODO: 2. Tetapkan parameter shape, margin, dan elevation dari Cari
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
+          borderRadius: BorderRadius.circular(10),
         ),
-        clipBehavior: Clip.antiAlias,
+        margin: EdgeInsets.all(4),
+        elevation: 1,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            //   TODO: 3. Buat Image sebagai anak dari Column
             Expanded(
-              child: Hero(
-                tag: 'candi-${candi.name}',
-                child: Image.asset(
-                  candi.imageAsset,
-                  fit: BoxFit.cover,
+              // TODO: 7. Implementasi Hero animation
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.asset(candi.imageAsset,
                   width: double.infinity,
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
+            //   TODO: 4. Buat Text sebagai anak dari Column
             Padding(
-              padding: const EdgeInsets.all(8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    candi.name,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    candi.location,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 13,
-                      color: Colors.grey[700],
-                    ),
-                  ),
-                ],
+              padding: EdgeInsets.only(left: 16, top: 8),
+              child: Text(candi.name,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            //   TODO: 5. Buat Text sebagai anak dari Column
+            Padding(
+              padding: EdgeInsets.only(left: 16, bottom: 8),
+              child: Text(candi.type,
+                style: TextStyle(
+                  fontSize: 12,
+                ),
               ),
             ),
           ],
